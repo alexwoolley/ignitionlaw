0e83799e89836e81dd85c129404ce53d8fe43f40
Alexander Woolley <amrwoolley@gmail.com>
2014-12-17 11:48:00 +0000

    Most functionality and some styling

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
-function enqueue_parent_theme_style() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+// add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
+// function enqueue_parent_theme_style() {
+//     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+// }
+
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header menu' ));
 }
+add_action( 'init', 'register_my_menu' );
