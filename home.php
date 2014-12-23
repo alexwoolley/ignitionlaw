0788ab63c4effaab6a15583e0f45df5b0e62a984
Alexander Woolley <amrwoolley@gmail.com>
2014-12-23 19:34:00 +0000

    Styling for email form

--- home.php
+++ home.php
@@ -18,6 +18,20 @@ Template Name: Home
 
 		<?php endwhile; ?>
 	<?php endif; ?>
-
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center">
+			<div id="home-email">
+				<button type="button">
+					Prefer email?
+				</button>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		jQuery('.wpcf7-form').hide();
+			jQuery('#home-email').click(function(){
+				jQuery('.wpcf7-form').toggle("slow")
+			});
+	</script>
 
 <?php get_footer(); ?>
