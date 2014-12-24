f11e0527dfb1c6d7f23d8be7ddb3dfea6730e4aa
Alexander Woolley <amrwoolley@gmail.com>
2014-12-24 11:46:05 +0000

    Last commit before moving on to multi-page website

--- home.php
+++ home.php
@@ -18,6 +18,50 @@ Template Name: Home
 
 		<?php endwhile; ?>
 	<?php endif; ?>
+	<script type="text/javascript">
+
+	if (jQuery('#alex-bio').css('display') == 'none') {
+		jQuery('#alex-photo').click(function(){
+			jQuery('#david-bio').hide("slow")
+			jQuery('#alex-bio').show("slow")
+			jQuery('html, body').stop().animate({
+				scrollTop: jQuery('#alex-bio').offset().top -80
+   				}, 2000);
+		});
+	}
+	//Obviously the problem is that the CSS is always set to hidden you idiot. 
+	else //if (jQuery('#alex-bio').css('display') == 'block')
+	{
+		jQuery('#alex-photo').click(function(){
+			jQuery('#alex-bio').hide("slow")
+			jQuery('html, body').stop().animate({
+				scrollTop: jQuery('#alex-photo').offset().top -80
+					}, 2000);
+		})
+	}
+
+		jQuery('#david-photo').click(function(){
+			jQuery('#alex-bio').hide("slow")
+			jQuery('#david-bio').toggle("slow")
+			jQuery('html, body').stop().animate({
+				scrollTop: jQuery('#david-bio').offset().top -80
+   				}, 2000);
+		});
+
+	// 	jQuery('#david-photo').bind('click', function(event) {
+	// 	var target = jQuery(this).attr('data-href');
+	// 	if (jQuery(this).is('a')) {
+	// 		target = jQuery(this).attr('href');
+	// 	}
+
+	// 	jQuery('html, body').stop().animate({
+	// 		scrollTop: jQuery(target).offset().top - 90
+	// 	}, 500);
+	// 	event.preventDefault();
+	// });
+	</script>
+
+
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center">
 			<div id="home-email">
@@ -28,10 +72,9 @@ Template Name: Home
 		</div>
 	</div>
 	<script type="text/javascript">
-		jQuery('.wpcf7-form').hide();
-			jQuery('#home-email').click(function(){
-				jQuery('.wpcf7-form').toggle("slow")
-			});
+		jQuery('#home-email').click(function(){
+			jQuery('.wpcf7-form').toggle("slow")
+		});
 	</script>
 
 <?php get_footer(); ?>
