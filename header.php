0f47a58426e878885b764a8f19c4e732424193be
Alexander Woolley <amrwoolley@gmail.com>
2015-01-09 10:26:03 +0000

    Better image for How we work page, plus title tag in header now reflects name of pages

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<title><?php wp_title( '|', true, 'right' ); ?>Ignition Law</title>
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css">
